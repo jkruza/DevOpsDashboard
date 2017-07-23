2d84f6fa3c3f2873df50e43f658eafa53682e249
Jarek Kruza <jarek@kruza.pl>
2017-07-23 13:01:38 +0200

    Enpoint for generic webhook

---
+++ DevOpsDashboard.Web/Controllers/API/WebHookGenericController.cs
@@ -0,0 +1,33 @@
+﻿using DevOpsDashboard.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DevOpsDashboard.Web.Controllers.API
+{
+    public class WebHookGenericController : ApiController
+    {
+        
+
+        // POST api/<controller>
+        [Route("api/webhook/generic")]
+        public DashboardMessageGeneric Post([FromBody]DashboardMessageGeneric msg)
+        {
+            return msg;
+        }
+        
+        [HttpPost]
+       [Route("api/webhook/generic/json")]
+        public async Task PostRawBufferManual()
+        {
+            string RawJSON = await Request.Content.ReadAsStringAsync();
+            DashboardMessageGeneric msg=DashboardMessageGeneric.CreateFromJson(RawJSON);
+            
+        }
+   
+    }
+}
