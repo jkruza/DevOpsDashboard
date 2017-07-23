18d96e2bdaec3663ee1a55c98fb3bb0dbaf03c3f
Jarek Kruza <baxterthehacker@users.noreply.github.com>
2017-07-23 20:50:26 +0200

    VSTS - specific handling for release completion

--- DevOpsDashboard.Model/DashboardMessageVSTS.cs
+++ DevOpsDashboard.Model/DashboardMessageVSTS.cs
@@ -14,13 +14,26 @@ namespace DevOpsDashboard.Model
             SourceData = EventPayload;
             Context = String.Empty;
             Timestamp = DateTime.Now;
+            Status = "Unknown";
 
             JObject json = JObject.Parse(EventPayload);
             
             Message = json["message"]["text"].ToString();
-            
             Title = "VSTS Event";
             Category = "General";
+
+            switch (json["eventType"].ToString())
+            {
+                case "ms.vss-release.deployment-completed-event":
+                    Category = "Release";
+                    Context = json["resource"]["deployment"]["releaseDefinition"]["id"] + "_" + json["resource"]["deployment"]["releaseEnvironment"]["id"];
+                    Title = json["resource"]["deployment"]["releaseDefinition"]["name"] + ": " + json["resource"]["deployment"]["releaseDefinition"]["name"];
+                    Status = json["resource"]["deployment"]["deploymentStatus"].ToString();
+                    break;
+
+            }
+
         }
     }
 }
+/
