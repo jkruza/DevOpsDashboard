02c552aa5fdbd56ba80a4484e772bccdec5b3c54
Jarek Kruza <jarek@kruza.pl>
2017-07-23 13:45:26 +0200

    Endpoint for GitHub webhook

---
+++ DevOpsDashboard.Web/Controllers/API/WebHookGitHubController.cs
@@ -0,0 +1,34 @@
+﻿using DevOpsDashboard.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DevOpsDashboard.Web.Controllers.API
+{
+    public class WebHookGitHubController : ApiController
+    {
+
+
+
+        [HttpPost]
+        [Route("api/webhook/github")]
+        public async Task<DashboardMessageGitHub> PostRawBufferManual()
+        {
+            
+            IEnumerable<string> headerValues = Request.Headers.GetValues("X-GitHub-Event");
+            string GitHubEvent = headerValues.FirstOrDefault();
+
+            string RawJSON = await Request.Content.ReadAsStringAsync();
+
+            DashboardMessageGitHub msg = new DashboardMessageGitHub(RawJSON, GitHubEvent);
+
+            return msg;
+
+        }
+
+    }
+}
