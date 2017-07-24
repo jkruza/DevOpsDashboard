fd5e855f004846a55c79d25bce85d99a0690fd53
Jarek Kruza <jarek@kruza.pl>
2017-07-24 23:35:19 +0200

    Send cache on connect and reconnect

--- DevOpsDashboard.Web/Hubs/DashboardMessageHub.cs
+++ DevOpsDashboard.Web/Hubs/DashboardMessageHub.cs
@@ -10,10 +10,24 @@ namespace DevOpsDashboard.Web.Hubs
     public class DashboardMessageHub : Hub
     {
         
+
         public void BroadcastDashboardMessage(DashboardMessageBase msg)
         {
             Clients.All.broadcastDashboardMessage(msg);
         }
-        
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            Clients.Caller.pushFromCache(DashboardMessageCache.GetAll());
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            Clients.Caller.pushFromCache(DashboardMessageCache.GetAll());
+            return base.OnConnected();
+        }
+
+
     }
 }
