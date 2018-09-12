9deca1cc8be4ca7d28e8a1d6bd192a09e9286352
Jarek Kruza <jarek@kruza.pl>
2018-09-12 23:21:46 +0200

    donet mv from template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevOpsDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
