0000cba1bf95f632303421dde36bfca566d4f761
Jarek Kruza <jarek@kruza.pl>
2018-09-13 20:36:54 +0200

    SignalR Hub ported

---
+++ Hubs/DashboardMessageHub.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using DevOpsDashboard.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace DevOpsDashboard.Hubs
+{
+    public class DashboardMessageHub : Hub
+    {
+        
+
+        public void BroadcastDashboardMessage(DashboardMessageBase msg)
+        {
+            Clients.All.SendAsync("broadcastDashboardMessage",msg);
+        }
+
+//PORT issue - support onConnect?
+/* 
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            Clients.Caller.pushFromCache(DashboardMessageCache.GetAll());
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            Clients.Caller.pushFromCache(DashboardMessageCache.GetAll());
+            return base.OnConnected();
+        }
+*/
+
+    }
+}
